e6147f801ceee021338fdf247509216261df46e9
geldenhuys.lynne@gmail.com <geldenhuys.lynne@gmail.com>
2022-03-28 15:40:03 +0200

    unused code removed

--- src/pages/jokelist.tsx
+++ src/pages/jokelist.tsx
@@ -171,28 +171,5 @@ const JokeList = () => {
     </div>
   );
 };
-      // const setJokes=()=>{
-      //   if(votes >= 15){
-      //     return "#4CAF50"
-      //   }
-      //   else if(votes >= 12){
-      //     return "#8BC34A"
-      //   }
-      //   else if(votes >= 9){
-      //     return "#CDDC39"
-      //   }
-      //   else if(votes >= 6){
-      //     return "#FFEB383"
-      //   }
-      //   else if(votes >= 3){
-      //     return "#FFC107"
-      //   }
-      //   else if(votes >= 0){
-      //     return "#FF9800"
-      //   }
-      //   else {
-      //     return "#f44336"
-      //   }
-      // }
 
 export default JokeList;
